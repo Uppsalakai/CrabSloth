0c03154e6f1f79d8803beb1dd9fc90138fcf574b
Carl Oscar Aaro <carloscar@agigen.se>
2012-06-12 20:38:08 +0200

    Cosplayer example file, we don't want to out all our internal data, when we go open source.

---
+++ cosplayers.example.js
@@ -0,0 +1,28 @@
+/* Cosplay data array goes here, rename this file to cosplayers.js and fill it with correct info */
+
+loadCosplayDataCallback([{
+    "index":1,
+    "id":1,
+    "name":"Luffy",
+    "source":"One Piece",
+    "rookie":false,
+    "user":{
+       "firstname":"Tobias",
+       "lastname":"Möllstam",
+       "city":"Stockholm",
+       "country":"Sverige"
+    }
+},
+{
+    "index":2,
+    "id":2,
+    "name":"L",
+    "source":"Deathnote",
+    "rookie":false,
+    "user":{
+       "firstname":"Carl Oscar",
+       "lastname":"A",
+       "city":"Uppsala",
+       "country":"Sverige"
+    }
+}]);
